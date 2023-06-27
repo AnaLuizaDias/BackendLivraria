86406830cd440e49c94731152a3f61af47a6c05d
Ana Luiza <analuizadiasdarocha212@gmail.com>
2023-06-27 15:00:28 -0300

    Criação autor.py

--- livraria/models.py
+++ livraria/models.py
@@ -15,15 +15,6 @@ class Editora(models.Model):
         return self.nome
 
 
-class Autor(models.Model):
-    nome = models.CharField(max_length=255)
-    email = models.EmailField(null=True, blank=True)
-
-    def __str__(self):
-        return self.nome
-
-    class Meta:
-        verbose_name_plural = "Autores"
 
 
 class Livro(models.Model):
